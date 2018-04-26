eb1ef4fe49a458cd562c14778e358cd25cfa5f3d
Brian Hatfield <bmhatfield@gmail.com>
2018-04-26 13:06:40 -0400

    Add TODOs for utils

---
+++ highlight/highlight.go
@@ -0,0 +1,5 @@
+package main
+
+func main() {
+	// TODO, take regexp, highlight lines that contain regex (like grep without filtering)
+}
---
+++ newlines/newlines.go
@@ -0,0 +1,5 @@
+package main
+
+func main() {
+	// TODO, automatically replace `\n` with `\\n` and vice-versa
+}
