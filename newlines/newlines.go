fea63850cb8914ad93760b41a0591c31729db74a
Brian Hatfield <bmhatfield@gmail.com>
2018-04-26 13:58:15 -0400

    Newlines: add initial newlines build

--- newlines/newlines.go
+++ newlines/newlines.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
 func main() {
-	// TODO, automatically replace `\n` with `\\n` and vice-versa
+	// Read data from stdin (caution, buffers all currently)
+	data, err := ioutil.ReadAll(os.Stdin)
+
+	// Write error to stderr and exit, if any.
+	if err != nil {
+		os.Stderr.WriteString(err.Error())
+		os.Exit(1)
+	}
+
+	// Use strings.replacer to flip \n to newline literals and vice-versa
+	replacer := strings.NewReplacer(`\n`, "\n", "\n", `\n`)
+	fmt.Println(replacer.Replace(string(data)))
 }
